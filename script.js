51c56c957db541ae64c6f0f34418aafe3ddbd297
Jacob Whisler <jwhisler521@gmail.com>
2019-12-13 16:49:40 -0500

    added document ready function tied to save button click

--- script.js
+++ script.js
@@ -1,2 +1,31 @@
 // sets the h4 element with id current time's text to the current date and time of day
 $('#currentTime').text(moment().format('dddd, MMMM Do h:mm:ss a'))
+
+
+
+// function tied to save button click that will pull the time and the text that the user typed into the textarea
+$(document).ready(function () {
+    $('saveBtn').on('click', function () {
+        var time = $(this).parent('div').attr('time');
+        var text = $(this).siblings('#col-md-10').val();
+
+
+     //  setting the variables to be saved in local storage
+        localStorage.setItem(time, text)
+
+    
+    })
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
